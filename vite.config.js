ae45f0973b8416065c1dc62c63e987d5ee4a271b
omer khan <omerfarooqkhan7210@gmail.com>
2024-07-03 16:44:40 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "./build",
+    emptyOutDir: true, // also necessary
+  },
+  resolve: {
+    alias: {
+      src: "/src",
+    },
+  },
   esbuild: {
     jsxFactory: 'React.createElement',
     jsxFragment: 'React.Fragment',
