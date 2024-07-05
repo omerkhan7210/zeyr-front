31f48b1a16b4376341b9b0c95d42f00ee8f04c1a
irma <irmakaleem12@gmail.com>
2024-07-05 17:02:55 +0500

    testing ci/cd

--- src/Components/Context/WishlistContext.jsx
+++ src/Components/Context/WishlistContext.jsx
@@ -1,9 +1,13 @@
 // src/Context/WishlistContext.js
-import { createContext ,useEffect,useState} from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { addToWishlist, removeFromWishlist, updateWishlistCount } from '../actions/wishlistActions';
-import axios from 'axios'
-import { hostLink } from '../Hostlink/hostlink';
+import { createContext, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addToWishlist,
+  removeFromWishlist,
+  updateWishlistCount,
+} from "../Actions/wishlistActions";
+import axios from "axios";
+import { hostLink } from "../Hostlink/hostlink";
 
 export const WishlistContextC = createContext();
 
@@ -14,94 +18,92 @@ const WishlistContext = ({ children }) => {
   const [wishlistItemsCount, setwishlistItemsCount] = useState(0); // State variable for total items count
   const [loading, setLoading] = useState(false);
 
-   // Fetch the user UUID from localStorage
-   const userUUID = localStorage.getItem('userUUID');
+  // Fetch the user UUID from localStorage
+  const userUUID = localStorage.getItem("userUUID");
 
-   const fetchwishlistItems = async () => {
+  const fetchwishlistItems = async () => {
     try {
       const response = await axios.get(`${hostLink}/wishlist/${userUUID}`);
       const wishlistItemsFromAPI = response.data.wishlistItems;
       // Filter out the wishlist items from the API that are already in Redux
       const filteredwishlistItemsFromAPI = wishlistItemsFromAPI.filter(
-        (apiItem) => !wishlistItemsFromRedux.some((reduxItem) => reduxItem.id === apiItem.prod_id)
+        (apiItem) =>
+          !wishlistItemsFromRedux.some(
+            (reduxItem) => reduxItem.id === apiItem.prod_id
+          )
       );
       // Merge the filtered wishlist items from Redux with the filtered wishlist items from the API
-      const mergedwishlistItems = await mergewishlistItems(wishlistItemsFromRedux, filteredwishlistItemsFromAPI);
-  
-      // Dispatch an action to update the Redux store with the fetched wishlist items
-      dispatch({ type: 'SET_wishlist_ITEMS', payload: mergedwishlistItems });
+      const mergedwishlistItems = await mergewishlistItems(
+        wishlistItemsFromRedux,
+        filteredwishlistItemsFromAPI
+      );
 
-       // Set the state variable with the merged wishlist items
-       return mergedwishlistItems;
+      // Dispatch an action to update the Redux store with the fetched wishlist items
+      dispatch({ type: "SET_wishlist_ITEMS", payload: mergedwishlistItems });
+
+      // Set the state variable with the merged wishlist items
+      return mergedwishlistItems;
     } catch (error) {
-     if (error.response) {
-       // The request was made, and the server responded with a status code
-       console.error(
-         "Server responded with status code:",
-         error.response.status
-       );
-       console.error("Response data:", error.response.data);
-     } else if (error.request) {
-       // The request was made, but no response was received
-       console.error("No response received. Check your server.");
-     } else {
-       // Something happened while setting up the request
-       console.error("Error setting up the request:", error.message);
-     }
+      if (error.response) {
+        // The request was made, and the server responded with a status code
+        console.error(
+          "Server responded with status code:",
+          error.response.status
+        );
+        console.error("Response data:", error.response.data);
+      } else if (error.request) {
+        // The request was made, but no response was received
+        console.error("No response received. Check your server.");
+      } else {
+        // Something happened while setting up the request
+        console.error("Error setting up the request:", error.message);
+      }
     }
   };
-   // Fetch wishlist items from your API based on the userUUID
-   useEffect(() => {
-    
+  // Fetch wishlist items from your API based on the userUUID
+  useEffect(() => {
     if (userUUID) {
-      const fetchData = async ()=>{
-       setLoading(true)
-       const witem = await  fetchwishlistItems();
-       setwishlistItems(witem)
-       setLoading(false)
-      }
-     fetchData();
+      const fetchData = async () => {
+        setLoading(true);
+        const witem = await fetchwishlistItems();
+        setwishlistItems(witem);
+        setLoading(false);
+      };
+      fetchData();
     }
- 
-   }, [userUUID, dispatch, wishlistItemsFromRedux]); // Include 'dispatch' and 'wishlistItemsFromRedux' as dependencies to trigger the effect whenever the Redux store is updated
- 
-   const mergewishlistItems = async (reduxItems, apiItems) => {
+  }, [userUUID, dispatch, wishlistItemsFromRedux]); // Include 'dispatch' and 'wishlistItemsFromRedux' as dependencies to trigger the effect whenever the Redux store is updated
+
+  const mergewishlistItems = async (reduxItems, apiItems) => {
     const wishlistItemsMap = new Map();
-  
+
     // Populate the wishlistItemsMap with Redux wishlist items
     reduxItems.forEach((item) => {
       wishlistItemsMap.set(item.id, { ...item }); // Clone the item to avoid modifying the Redux state
     });
-  
+
     try {
       const fetchProductPromises = apiItems.map((apiItem) => {
         return axios.get(`${hostLink}/products/${apiItem.prod_id}`);
       });
-  
+
       const productResponses = await Promise.all(fetchProductPromises);
-  
+
       apiItems.forEach((apiItem, index) => {
-        
-          // If the wishlist item does not exist in Redux, add it to the wishlistItemsMap
-          wishlistItemsMap.set(apiItem.prod_id, {
-            ...apiItem,
-            id: apiItem.prod_id, // Set the id to prod_id to match Redux structure
-          });
-        
-      
+        // If the wishlist item does not exist in Redux, add it to the wishlistItemsMap
+        wishlistItemsMap.set(apiItem.prod_id, {
+          ...apiItem,
+          id: apiItem.prod_id, // Set the id to prod_id to match Redux structure
+        });
+
         // Append the product details to the wishlist item
         const productDetails = productResponses[index].data.product;
         // Update the merged wishlist item in the map
         wishlistItemsMap.set(apiItem.prod_id, productDetails);
       });
-      
-    } catch (error) {
-     
-    }
+    } catch (error) {}
     // Extract the values of the wishlistItemsMap to get the merged wishlist items
     return Array.from(wishlistItemsMap.values());
   };
-
 
   const addToWishlistHandler = (product) => {
     dispatch(addToWishlist(product));
@@ -113,8 +115,7 @@ const WishlistContext = ({ children }) => {
 
   // Calculate the wishlist items count and update the Redux store
   useEffect(() => {
-    if(wishlist){
-
+    if (wishlist) {
       const totalCount = wishlist.length;
       setwishlistItemsCount(wishlist.length);
       dispatch(updateWishlistCount(totalCount));
